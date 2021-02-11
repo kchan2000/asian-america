afc89644244a0d1dfeb48c7756094858e0538fbc
kchan2000 <katiechan@Katies-MacBook-Pro.local>
2021-02-11 02:07:02 -0500

    woooohooo

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+$(document).ready(function() {
+  
+  // put javascript code here
+  console.log("page has loaded!");
+  
+  $(function () {
+        $("#japanese, #lao, #viet, #taiwan, #filipino, #chinese,#srilankan, #indian, #malay, #indo, #pakistani, #thai, #bangladeshi, #hmong, #korean, #cambodian").draggable();
+    });
+    
+    
+    $('#homeanswer').click(function () {
+        $('#hometext').toggle();
+    });
+    
+     $('#povertyanswer').click(function () {
+        $('#povertytext').toggle();
+    });
+    
+    
+     $('#hsanswer').click(function () {
+        $('#hstext').toggle();
+    });
+    
+    
+    
+});
 
